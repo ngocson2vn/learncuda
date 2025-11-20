62cc92e750912ccc96dce35af442e59274214bc6
Son Nguyen <ngocson2vn@gmail.com>
2025-11-20 09:19:26 +0000

    Update cutlass

--- docs/01commands.sh
+++ docs/01commands.sh
@@ -5,6 +5,8 @@
 # GPU Memory Utilization of GPU0
 while true; do nvidia-smi --id=0 --query-gpu=timestamp,memory.total,memory.used,memory.free --format=csv && echo && sleep 5; done
 
+nvidia-smi -i 0 -l 1
+
 # QUERY GPU METRICS FOR HOST-SIDE LOGGING
 nvidia-smi --query-gpu=timestamp,name,pci.bus_id,driver_version,pstate,pcie.link.gen.max,pcie.link.gen.current,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 5
 
