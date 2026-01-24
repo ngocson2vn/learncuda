041561f92d2060b8c7148d9cd4dd5b2d123fac4f
Son Nguyen <ngocson2vn@gmail.com>
2026-01-24 03:51:51 +0000

    Refactoring

--- src/matmul_wgmma/gen_cxx.py
+++
@@ -1,2 +0,0 @@
-for i in range(0, 32):
-  print("  int idx{0:02d} = indexes[{1:d}];".format(i, i))
--- src/matmul_wgmma/perf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo /usr/local/cuda/bin/ncu -f -o report_wgmma_with_tma ./wgmma_with_tma 16 32 16
